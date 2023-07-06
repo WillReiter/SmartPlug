f0a5470469e7e7fd9cb61f012074c765cc4f286e
Will Reiter <will.reiter6@gmail.com>
2023-07-05 22:05:28 -0500

    Initial commit

---
+++ Connection/connect.py
@@ -0,0 +1,11 @@
+# my_ip holds value of ip address from ip_address.py
+
+from ip_address import ip_address as my_ip
+from ip_address import ip_lst as my_ip_addresses
+
+
+
+print("ip list:\n", my_ip_addresses)
+
+
+#print(my_ip)
---
+++ Connection/ip_address.py
@@ -0,0 +1,8 @@
+
+
+f = open("Connection/inputs/ip_input.txt", "r")
+ip_lst = f.read()
+print(ip_lst)
+
+
+ip_address = '192.168.0.0'
